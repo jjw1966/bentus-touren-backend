1c2324ca4312640294f72a5f29e70a7792275ba0
jjw1966 <joachim.j.wilhelm@gmail.com>
2026-07-06 01:00:06 +0200

    Update range for deltävlingar sheets from 1-8 to 5-8

--- api/deltavlingar.py
+++ api/deltavlingar.py
@@ -5,6 +5,6 @@ deltavlingar_bp = Blueprint("deltavlingar", __name__, url_prefix="/deltavlingar"
 
 @deltavlingar_bp.route("/")
 def get_deltavlingar():
-    sheets = [f"Deltävling {i}" for i in range(1, 9)]
+    sheets = [f"Deltävling {i}" for i in range(5, 9)]
     output = {s: read_sheet(s) for s in sheets}
     return jsonify(output)
