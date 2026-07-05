43997703272bd21a47807dbbfaf4ea916eb28812
jjw1966 <joachim.j.wilhelm@gmail.com>
2026-07-06 00:56:49 +0200

    Replace blueprints and update main application

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 from flask import Flask
 from flask_cors import CORS
 
-from api.resultat import resultat_bp
-from api.spelare import spelare_bp
-from api.lagspel import lagspel_bp
+from api.dashboard import dashboard_bp
 from api.tourstallning import tourstallning_bp
+from api.lagspel import lagspel_bp
 from api.deltavlingar import deltavlingar_bp
 
 app = Flask(__name__)
 CORS(app)
 
-app.register_blueprint(resultat_bp)
-app.register_blueprint(spelare_bp)
-app.register_blueprint(lagspel_bp)
+app.register_blueprint(dashboard_bp)
 app.register_blueprint(tourstallning_bp)
+app.register_blueprint(lagspel_bp)
 app.register_blueprint(deltavlingar_bp)
 
 @app.route("/")
