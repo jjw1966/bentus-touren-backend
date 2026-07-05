f4c8d009571e25cb439e3c8c2fd26bde81881093
jjw1966 <joachim.j.wilhelm@gmail.com>
2026-07-05 23:32:44 +0200

    Update spelare endpoint to use read_sheet
    Refactored player endpoint to use read_sheet function.

--- api/spelare.py
+++ api/spelare.py
@@ -1,22 +1,9 @@
 from flask import Blueprint, jsonify
-from data.drive_connector import download_excel_from_drive
-from data.excel_reader import read_excel_data
+from data.excel_reader import read_sheet
 
-# Skapa blueprint för spelare
-app = Blueprint("spelare", __name__)
+spelare_bp = Blueprint("spelare", __name__, url_prefix="/spelare")
 
-@app.route("/spelare")
-def spelare():
-    """
-    Endpoint som returnerar spelarprofiler från Excel-filen.
-    """
-    try:
-        file_path = download_excel_from_drive()
-        sheets = read_excel_data(file_path)
-
-        # Plocka ut bladet "spelare" om det finns
-        spelare_data = sheets.get("spelare", {})
-        return jsonify(spelare_data)
-
-    except Exception as e:
-        return jsonify({"error": str(e)})
+@spelare_bp.route("/")
+def get_spelare():
+    data = read_sheet("Spelare")
+    return jsonify(data)
