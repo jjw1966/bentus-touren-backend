e3a905dcc2614921eacc2989a0909500246be5ad
jjw1966 <joachim.j.wilhelm@gmail.com>
2026-07-06 00:57:49 +0200

    Change dashboard route prefix and data source

--- api/dashboard.py
+++ api/dashboard.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, jsonify
 from data.excel_reader import read_sheet
 
-dashboard_bp = Blueprint("dashboard", __name__, url_prefix="/dashboard")
+dashboard_bp = Blueprint("dashboard", __name__, url_prefix="/resultat")
 
 @dashboard_bp.route("/")
 def get_dashboard():
-    data = read_sheet("Resultat")
+    data = read_sheet("Dashboard")
     return jsonify(data)
