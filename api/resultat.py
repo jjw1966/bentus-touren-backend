1bf9ac48e7e2007ec17cb0ddd5a2d1b70684a5a6
jjw1966 <joachim.j.wilhelm@gmail.com>
2026-07-05 23:59:21 +0200

    Add Flask blueprint for resultat endpoint

---
+++ api/resultat.py
@@ -0,0 +1,9 @@
+from flask import Blueprint, jsonify
+from data.excel_reader import read_sheet
+
+resultat_bp = Blueprint("resultat", __name__, url_prefix="/resultat")
+
+@resultat_bp.route("/")
+def get_resultat():
+    data = read_sheet("Resultat")
+    return jsonify(data)
