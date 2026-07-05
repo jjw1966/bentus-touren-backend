6a23e3c764e084a6b7709a4ae6f396230f14d9b9
jjw1966 <joachim.j.wilhelm@gmail.com>
2026-07-06 00:00:34 +0200

    Update Flask app to include resultat_bp blueprint

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 from flask import Flask
 from flask_cors import CORS
 
-# Importera alla API‑blueprints
-from api.dashboard import dashboard_bp
-from api.deltavlingar import deltavlingar_bp
-from api.lagspel import lagspel_bp
+from api.resultat import resultat_bp
 from api.spelare import spelare_bp
+from api.lagspel import lagspel_bp
 from api.tourstallning import tourstallning_bp
+from api.deltavlingar import deltavlingar_bp
 
 app = Flask(__name__)
 CORS(app)
 
-# Registrera endpoints
-app.register_blueprint(dashboard_bp)
-app.register_blueprint(deltavlingar_bp)
-app.register_blueprint(lagspel_bp)
+app.register_blueprint(resultat_bp)
 app.register_blueprint(spelare_bp)
+app.register_blueprint(lagspel_bp)
 app.register_blueprint(tourstallning_bp)
+app.register_blueprint(deltavlingar_bp)
 
 @app.route("/")
 def home():
