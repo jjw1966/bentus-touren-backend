3a0b436fd568c34240c0f1c59d399866a1e4cf88
jjw1966 <joachim.j.wilhelm@gmail.com>
2026-07-06 00:59:36 +0200

    Update get_lagspel to return multiple sheets data

--- api/lagspel.py
+++ api/lagspel.py
@@ -5,5 +5,6 @@ lagspel_bp = Blueprint("lagspel", __name__, url_prefix="/lagspel")
 
 @lagspel_bp.route("/")
 def get_lagspel():
-    data = read_sheet("Lagspel")
-    return jsonify(data)
+    sheets = ["Sura", "Fullerö", "Strand 1", "Strand 2"]
+    output = {s: read_sheet(s) for s in sheets}
+    return jsonify(output)
