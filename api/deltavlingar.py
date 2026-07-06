232feb00f064aae539d53b0f79fcfac70cab7090
jjw1966 <joachim.j.wilhelm@gmail.com>
2026-07-06 22:31:37 +0200

    Refactor deltavlingar route and import structure

--- api/deltavlingar.py
+++ api/deltavlingar.py
@@ -1,10 +1,9 @@
 from flask import Blueprint, jsonify
-from data.excel_reader import read_sheet
+from excel_reader import read_sheet
 
-deltavlingar_bp = Blueprint("deltavlingar", __name__, url_prefix="/deltavlingar")
+app = Blueprint("deltavlingar", __name__)
 
-@deltavlingar_bp.route("/")
-def get_deltavlingar():
-    sheets = [f"Deltävling {i}" for i in range(5, 9)]
-    output = {s: read_sheet(s) for s in sheets}
-    return jsonify(output)
+@app.route("/deltavlingar")
+def deltavlingar():
+    data = read_sheet("Deltävlingar")
+    return jsonify(data)
