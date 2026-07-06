0e25490585c8c3c00f55c2bb32b20c634e97b69f
jjw1966 <joachim.j.wilhelm@gmail.com>
2026-07-06 23:04:13 +0200

    Fix import path for read_sheet function

--- api/dashboard.py
+++ api/dashboard.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify
-from data.excel_reader import read_sheet
+from excel_reader import read_sheet
 
 dashboard_bp = Blueprint("dashboard", __name__, url_prefix="/resultat")
 
